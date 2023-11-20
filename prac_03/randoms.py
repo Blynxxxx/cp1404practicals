054f5e62d7fc6a2d67e5ac23920d862e5737361a
linhvan <baolinh.van@my.jcu.edu.au>
2023-11-20 17:44:32 +0800

    Add randoms practical

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+"""
+What did you see on line 1? 15
+What was the smallest number you could have seen, what was the largest? 5 - 20
+
+What did you see on line 2? 7
+What was the smallest number you could have seen, what was the largest? 3 - 9
+Could line 2 have produced a 4? No
+
+What did you see on line 3? 4.159229824821782
+What was the smallest number you could have seen, what was the largest? 2.5 - 5.5
+"""
+import random
+
+print(random.uniform(1,100))
