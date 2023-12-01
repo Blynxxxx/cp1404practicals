ba1c0476d92ee68308304e2602ac4fd56f71ab72
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-01 14:24:19 +0800

    Modify variable name as naming convention

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -35,13 +35,13 @@ lowercase_full_names = [name.lower() for name in full_names]
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
-numbers = [int(number) for number in almost_numbers]
+numbers = [int(almost_number) for almost_number in almost_numbers]
 # print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
-greater_nine_numbers = [number for number in numbers if number > 9]
-# print(greater_nine_numbers)
+big_numbers = [number for number in numbers if number > 9]
+# print(big_numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
