59009131a5ac39f0111422eb0b817434ecc25052
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-01 14:17:45 +0800

    Modify function get_subjects

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,18 +7,24 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    subjects = get_data()
+    """Read subject data and display."""
+    subjects = get_subjects()
     # print(subjects)
     display_subjects(subjects)
 
 
-def get_data():
+def get_subjects():
     """Read data from file formatted like: subject,lecturer,number of students."""
     subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        parts = line.strip().split(",")
-        parts[2] = int(parts[2])
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
         subject.append(parts)
     input_file.close()
     return subject
