ccaccdb966e571d7c0dd463374929ddebb24468e
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-04 17:44:06 +0800

    Complete languages prac

---
+++ prac_06/programming_language.py
@@ -0,0 +1,12 @@
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection=True, year=0):
+        self.name = name
+        self.typing = typing.title()
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name},{self.typing}, Reflection = {self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
