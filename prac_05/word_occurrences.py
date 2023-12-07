1a0ab31ff901908d81dc8791660c71449cb629dd
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-07 17:28:02 +0800

    Check again for the final code

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,5 +8,6 @@ words.sort()
 longest_length = max(len(word) for word in words)
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
+
 for word, count in word_to_count.items():
     print(f"{word:{longest_length}}: {count}")
