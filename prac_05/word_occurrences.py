1fd7465e776bf8a265677976b15a8c20cb4d0291
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-07 21:52:32 +0800

    Modify the name variable for word length

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,9 +5,9 @@ Word Occurrences
 word_to_count = {}
 words = (input("Text: ")).strip().split(" ")
 words.sort()
-longest_length = max(len(word) for word in words)
+maximum_word_length = max(len(word) for word in words)
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
 for word, count in word_to_count.items():
-    print(f"{word:{longest_length}}: {count}")
+    print(f"{word:{maximum_word_length}}: {count}")
