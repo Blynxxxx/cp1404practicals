cf9632c99c74a62dcc26eda9190046af3606d670
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-07 17:26:06 +0800

    create functions for the code

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,17 +2,28 @@
 CP1404/CP5632 Practical
 Emails
 """
-email_to_name = {}
-email = input("Email: ")
-while email != "":
-    name = " ".join(email[:email.find("@")].split(".")).title()
-    check_name_question = input(f"Is your name {name}? (Y/n) ").upper()
-    if check_name_question != "Y" and check_name_question != "":
-        name = input("Name: ").title()
-    email_to_name[email] = name
+
+
+def main():
+    """Create dictionary of emails-to-names."""
+    email_to_name = {}
     email = input("Email: ")
-print()
-for email,name in email_to_name.items():
-    print(f"{name} ({email})")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").upper()
+        if confirmation != "Y" and confirmation != "":
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input("Email: ")
+    print()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
+def get_name_from_email(email):
+    """Get name from the email."""
+    name = " ".join(email[:email.find("@")].split(".")).title()
+    return name
+
+
+main()
