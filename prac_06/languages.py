4b115a65a3238e730b90808438252c84f1fdeecd
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-09 22:10:19 +0800

    Complete programing language prac6

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,14 +1,22 @@
 """
-17:20 - 17:43
+CP1404/CP5632 Practical
+Programming Language tests.
+17:20 - 17: 43
 """
 from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
-languages = [python,ruby,visual_basic]
-print("The dynamically typed languages are:")
-for language in languages:
-    if language.is_dynamic():
-        print(language.name)
+
+def main():
+    """Display programming languages which are dynamic."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    # print(python)
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,23 @@
+"""
+CP1404/CP5632 Practical
+Programming Language class.
+"""
+
+
 class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
     def __init__(self, name="", typing="", reflection=True, year=0):
+        """Initialise a programming language instance with given values."""
         self.name = name
-        self.typing = typing.title()
+        self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
-        return f"{self.name},{self.typing}, Reflection = {self.reflection}, First appeared in {self.year}"
+        """Display programming language details."""
+        return f"{self.name}, {self.typing}, Reflection = {self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """Determine whether programming language is dynamic or not."""
         return self.typing == "Dynamic"
