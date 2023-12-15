da9953468602497414e607a7e84bfd751d5c7515
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-15 23:17:34 +0800

    Create basic Project object

---
+++ prac_07/project.py
@@ -0,0 +1,14 @@
+"""
+Project Management Program
+22:40 -
+"""
+class Project:
+    """Represent a Project object."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise a Project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
