d0c8ba5ab3ff4b1d32ee9b49470421f4094dc0cb
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-16 12:48:40 +0800

    Format datetime for start date

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,8 +1,11 @@
 """
 Project Management Program
 22:40 - 2:00
+12:15 - 13:00
 """
+import datetime
 COMPLETION_NUMBER = 100
+
 
 class Project:
     """Represent a Project object."""
@@ -10,15 +13,15 @@ class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         """Initialise a Project instance."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
     def __str__(self):
         """Display Project information."""
-        return f"{self.name}, start: {self.start_date}, priority {self.priority}," \
-               f" estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
+        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}," \
+        f" estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
 
     def __lt__(self, other):
         return self.priority < other.priority
