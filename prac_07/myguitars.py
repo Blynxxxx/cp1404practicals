26d8c9edd30f3351d30c83e0f7623d5fcfc78087
linhvan <baolinh.van@my.jcu.edu.au>
2023-12-16 19:13:17 +0800

    Complete practical

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,21 +4,15 @@ FILENAME = "guitars.csv"
 
 
 def main():
+    """Load guitar file, display, get new guitars and save."""
     guitars = []
-    # with open(FILENAME, "r") as in_file:
-    #     for row in in_file:
-    #         # print(row)
-    #         parts = row.strip().split(",")
-    #         year = parts[1]
-    #         cost = float(parts[2])
-    #         guitars.append(Guitar(parts[0], year, cost))
-    # guitars.sort()
-    # for guitar in guitars:
-    #     print(guitar)
-    # guitars.sort()
+    load_guitar_file(guitars)
+
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
 
     print("My guitars!")
-
     guitar_name = input("Name: ")
     while guitar_name != "":
         guitar_year = int(input("Year: "))
@@ -27,9 +21,26 @@ def main():
         guitars.append(guitar)
         print(f"{guitar} added")
         guitar_name = input("Name: ")
+
+    save_guitar_file(guitars)
+
+
+def save_guitar_file(guitars):
+    """Save list of guitars to file."""
     with open(FILENAME, "a") as out_file:
         for guitar in guitars:
             print(guitar, file=out_file)
+
+
+def load_guitar_file(guitars):
+    """Load guitar file."""
+    with open(FILENAME, "r") as in_file:
+        for row in in_file:
+            # print(row)
+            parts = row.strip().split(",")
+            year = parts[1]
+            cost = float(parts[2])
+            guitars.append(Guitar(parts[0], year, cost))
 
 
 main()
