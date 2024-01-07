e1d69fb571bb6ea926c9d24ce32f716ab9ebd3ff
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-07 18:15:47 +0800

    create main function

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,29 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+from prac_09.taxi import Taxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            choose_taxi(taxis)
+        elif choice == "D":
+            drive_taxi(current_taxi)
+
+        else:
+            print("Invalid option")
+        display_bill_to_date(current_taxi)
+        print(MENU)
+        choice = input(">>> ").upper()
+    pass
+
+
+
+
+main()
