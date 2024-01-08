19c74a8bf9ed63c895a2002c9dcc52936cbe3e7b
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-08 11:22:47 +0800

    Modify the drive function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,39 +7,51 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
+    total_bill = 0
     print("Let's drive!")
     print(MENU)
-    choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "C":
-            choose_taxi(taxis)
-        elif choice == "D":
-            drive_taxi(current_taxi)
-
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available:")
+            display_taxis(taxis)
+            current_taxi = choose_taxi(taxis)
+        elif menu_choice == "d":
+            if current_taxi:
+                current_taxi.start_fare()
+                distance = float(input("Drive how far? "))
+                current_taxi.drive(distance)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost: .2f}")
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
-        display_bill_to_date(current_taxi)
+        display_total_bill(total_bill)
         print(MENU)
-        choice = input(">>> ").upper()
-    pass
+        menu_choice = input(">>> ").lower()
+    display_total_bill(total_bill)
+    print("Taxis are now:")
+    display_taxis(taxis)
 
 
-def drive_taxi(current_taxi):
-    if current_taxi:
-        return current_taxi.drive()
-    print("You need to choose a taxi before you can drive")
+def display_total_bill(total_bill):
+    print(f"Bill to date: ${total_bill:.2f}")
 
 
-def choose_taxi(taxis):
-    print("Taxis available:")
+def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
 
-def display_bill_to_date(current_taxi):
-    if current_taxi:
-        fare = current_taxi.get_fare()
-    else:
-        fare = 0
-    print(f"Bill to date: ${fare :.2f}")
+
+def choose_taxi(taxis):
+    taxi_choice = int(input("Choose taxi: "))
+    try:
+        current_taxi = taxis[taxi_choice]
+        return current_taxi
+    except IndexError or ValueError:
+        print("Invalid taxi choice")
+
 
 main()
