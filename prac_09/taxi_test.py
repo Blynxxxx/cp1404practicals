e03535c146a1bd6c44b34c6d7fed83742e76aff5
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-11 00:09:42 +0800

    Complete the test taxi

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -6,12 +6,12 @@ def main():
     my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
-    print(my_taxi.get_fare())
+    print(f"trip cost ${my_taxi.get_fare()}")
 
     my_taxi.start_fare()
     my_taxi.drive(100)
     print(my_taxi)
-    print(my_taxi.get_fare())
+    print(f"trip cost  ${my_taxi.get_fare()}")
 
 
 main()
