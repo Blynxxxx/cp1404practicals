e0ea3261bf4a1e7768d2939fad2c47a201792cda
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-11 00:10:38 +0800

    Add docstring for all functions

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,16 +2,19 @@ from prac_09.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    """Represent a SilverServiceTaxi object."""
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= self.fanciness
 
     def get_fare(self):
-        """Return the price for the silver service taxi trip."""
+        """Get the price for the silver service taxi trip."""
         return super().get_fare() + self.flagfall
 
     def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall: .2f}"
