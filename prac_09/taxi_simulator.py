c8f8bf51634543c81c83e786ed6c8e70998eb89f
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-10 23:16:34 +0800

    Add docstring for all functions

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,3 +1,4 @@
+from prac_06.car import Car
 from prac_09.silver_service_taxi import SilverServiceTaxi
 from prac_09.taxi import Taxi
 
@@ -5,6 +6,8 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    """Create taxi simulator program which user can choose taxis, how far they want to drive,
+    display the trip cost and total bill."""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
     total_bill = 0
@@ -37,21 +40,26 @@ def main():
 
 
 def display_total_bill(total_bill):
+    """Display total bill."""
     print(f"Bill to date: ${total_bill:.2f}")
 
 
 def display_taxis(taxis):
+    """Display numbered list of taxis."""
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
 
 
 def choose_taxi(taxis):
+    """Get valid taxi choice."""
     taxi_choice = int(input("Choose taxi: "))
     try:
         current_taxi = taxis[taxi_choice]
         return current_taxi
-    except IndexError or ValueError:
+    except IndexError:
         print("Invalid taxi choice")
+    except ValueError:
+        print("Taxi choice should be number")
 
 
 main()
