0fa84e64e81024b628c07cfa47a09ef83ffecdc2
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-11 21:24:36 +0800

    Complete testing for unreliable car

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -5,19 +5,19 @@ def main():
     """Test some UnreliableCars."""
 
     # create cars with different reliabilities
-    good_car = UnreliableCar("Mostly Good", 100, 90)
-    bad_car = UnreliableCar("Dodgy", 100, 9)
+    my_car = UnreliableCar("My Car", 100, 90)
+    limo = UnreliableCar("Limo", 100, 9)
 
     # attempt to drive the cars many times
     # output what distance they drove
-    for i in range(1, 12):
+    for i in range(1, 11):
         print(f"Attempting to drive {i}km:")
-        print(f"{good_car.name} drove {good_car.drive(i)}km")
-        print(f"{bad_car.name} drove {bad_car.drive(i)}km")
+        print(f"{my_car.name} drove {my_car.drive(i)}km")
+        print(f"{limo.name} drove {limo.drive(i)}km")
 
     # print the final states of the cars
-    print(good_car)
-    print(bad_car)
+    print(my_car)
+    print(limo)
 
 
 main()
