e783dc584a690dea2998ee4a3c108859d024d333
linhvan <baolinh.van@my.jcu.edu.au>
2024-01-11 21:23:42 +0800

    Add docstring for main function

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,17 +3,16 @@ from prac_09.car import Car
 
 
 class UnreliableCar(Car):
-    """ """
+    """Represent a UnreliableCar object which is inherited from Car class."""
 
     def __init__(self, name, fuel, reliability):
-        """Initialise a Unreliable Car instance, based on parent class Car."""
+        """Initialise a Unreliable Car instance, based on parent Car class."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
         """Drive the car if a random number is less than the car's reliability."""
-        if random.uniform(0, 100) < self.reliability:
-            return super().drive(distance)
-
-        else:
-            return 0
+        if random.uniform(0, 100) >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
